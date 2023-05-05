83cd55d43ed14c345709e4e1ab8d10cb4bc8db5b
Talha-Altair <cooltalha27@gmail.com>
2023-05-06 02:47:40 +0530

    Updates for 2023-05-06 02:47:40

--- app.py
+++ app.py
@@ -3,9 +3,26 @@ from settings import HOST, PORT, BACKEND_URL
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/dashboard/<string:username>")
+def index(username):
+
+    data = {
+        'basic': {
+            'name': 'Dr. Balaji Ramanujam',
+            'designation': 'Professor',
+            'email': 'balaji.cse@crescent.education',
+            'doj': '21-09-2017',
+            'exp': '12 Years',
+            'research': [
+                'Data Mining',
+                'Machine Learning',
+                'Big Data Analytics',
+                'Cloud Computing'
+            ]
+        }
+    }
+
+    return render_template("index.html", data = data)
 
 
 if __name__ == "__main__":
