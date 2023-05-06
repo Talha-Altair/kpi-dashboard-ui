e2c4650a4efc8da578da47a9af5d34a12b4c2424
Talha-Altair <cooltalha27@gmail.com>
2023-05-06 06:37:52 +0530

    Updates for 2023-05-06 06:37:52

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/")
+def login():
+
+    return render_template("base.html")
+
 
 @app.route("/dashboard/<string:username>")
 def index(username):
+
     data = {
         "username": username,
         "basic": {
@@ -30,6 +36,7 @@ def index(username):
 
 @app.route("/dashboard/<string:username>/academics")
 def academic(username):
+
     data = {
         "username": username,
         "charts": {
@@ -38,11 +45,76 @@ def academic(username):
                 ["Green Design", 78],
                 ["Operating System", 94],
                 ["Cloud Computing", 69],
-            ]
+            ],
+            "ques_quality_data": {
+                "base_data": [
+                    {"name": "compiler", "y": 52.74, "drilldown": "compiler"},
+                    {"name": "Green Design", "y": 72.74, "drilldown": "Green Design"},
+                    {
+                        "name": "Operating System",
+                        "y": 62.74,
+                        "drilldown": "Operating System",
+                    },
+                    {
+                        "name": "Cloud Computing",
+                        "y": 82.74,
+                        "drilldown": "Cloud Computing",
+                    },
+                ],
+                "drilldown_data": [
+                    {
+                        "name": "compiler",
+                        "id": "compiler",
+                        "data": [["cat1", 47], ["cat2", 50], ["endsem", 75]],
+                    },
+                    {
+                        "name": "Green Design",
+                        "id": "Green Design",
+                        "data": [["cat1", 87], ["cat2", 90], ["endsem", 85]],
+                    },
+                    {
+                        "name": "Operating System",
+                        "id": "Operating System",
+                        "data": [["cat1", 87], ["cat2", 90], ["endsem", 85]],
+                    },
+                    {
+                        "name": "Cloud Computing",
+                        "id": "Cloud Computing",
+                        "data": [["cat1", 87], ["cat2", 90], ["endsem", 85]],
+                    },
+                ],
+            },
+            "material_quality_data": [
+                ["Compiler", 66],
+                ["Green Design", 18],
+                ["Operating System", 98],
+                ["Cloud Computing", 69],
+            ],
+        },
+        "basic": {
+            "name": "Dr. Balaji Ramanujam",
+            "designation": "Professor",
+            "email": "balaji.cse@crescent.education",
+            "doj": "21-09-2017",
+            "exp": "12 Years",
+            "research": [
+                "Data Mining",
+                "Machine Learning",
+                "Big Data Analytics",
+                "Cloud Computing",
+            ],
+            "teaching_methods": [
+                "Slow Paced Lectures",
+                "Focus onCase Study",
+                "Frequent Group Discussion",
+                "Real time Problem Solving",
+            ],
         },
     }
 
-    return render_template("academics.html", data=data, json_data = json.dumps(data), active="academics")
+    return render_template(
+        "academics.html", data=data, json_data=json.dumps(data), active="academics"
+    )
 
 
 if __name__ == "__main__":
