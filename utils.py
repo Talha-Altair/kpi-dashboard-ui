17faeebe655ee2a7cf01da0d769a8bf2a9a28197
Talha-Altair <cooltalha27@gmail.com>
2023-05-12 15:01:15 +0530

    Updates for 2023-05-12 15:01:15

--- utils.py
+++ utils.py
@@ -1,13 +1,16 @@
 import requests
 from settings import BACKEND_URL
 
-def get_dashboard_data(username) -> dict:
+def get_dashboard_data(username):
 
     res = requests.get(f"{BACKEND_URL}/dashboard/{username}")
 
     if res.status_code == 200:
 
+        print(res.json())
+
         return res.json()
+    
     
     else:
 
